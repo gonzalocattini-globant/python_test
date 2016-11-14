39ab9852b745335aeee2618cb94eff546231eacf
gonzalo.cattini <gonzalo.cattini@globant.com>
2016-11-14 13:55:41 -0300

    test script tested

--- bin/Test selenium script.py
+++ bin/Test selenium script.py
@@ -1,11 +1,10 @@
 import os
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 chromedriver = "C:\\Users\\gonzalo.cattini\\IdeaProjects\\python_test\\resources\\chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 
+#Add the chorme driver to the instance
 driver = webdriver.Chrome(chromedriver)
 
 driver.get("http://www.google.com")
